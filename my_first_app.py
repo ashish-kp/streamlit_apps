15e7828cc36f6eecf21410868c18e6e441807214
Ashish <75926474+ashish-kp@users.noreply.github.com>
2022-05-25 23:35:16 +0530

    Update my_first_app.py

--- my_first_app.py
+++ my_first_app.py
@@ -20,7 +20,6 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
-from googlesearch import search
 import sys
 
 headers = {
