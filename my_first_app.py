e9a7be26d675596e02508a0672a09509b178c070
Ashish <75926474+ashish-kp@users.noreply.github.com>
2022-05-25 23:51:23 +0530

    Update my_first_app.py

--- my_first_app.py
+++ my_first_app.py
@@ -44,35 +44,36 @@ if inp != None:
     soup = bs(page.content, 'html.parser')
 
     content = soup.find('div', class_ = 'L_gH')
+    
+    if content != None:
+        all_links = content.select('a')
 
-    all_links = content.select('a')
+        store_links = []
 
-    store_links = []
+        count = 3
+        for link in all_links:
+            if count % 4 == 0:
+                store_links.append(link['href'])
+            count += 1
 
-    count = 3
-    for link in all_links:
-        if count % 4 == 0:
-            store_links.append(link['href'])
-        count += 1
+        st.text('Links are retrieved. Now the text...')
 
-    st.text('Links are retrieved. Now the text...')
+        stories = []
 
-    stories = []
+        for link in store_links:
+            page = requests.get(link, headers = headers)
+            soup = bs(page.content, 'html.parser')
+            story = soup.select('div.panel.article.aa_eQ')
+            stories.append(story[0].text)
 
-    for link in store_links:
-        page = requests.get(link, headers = headers)
-        soup = bs(page.content, 'html.parser')
-        story = soup.select('div.panel.article.aa_eQ')
-        stories.append(story[0].text)
+        all = ''.join(stories)
 
-    all = ''.join(stories)
+        st.text('Here\'s the WordCloud:')
 
-    st.text('Here\'s the WordCloud:')
+        wordcloud = WordCloud(stopwords = stop,background_color = 'white',width=2500,height=2000).generate(all)
 
-    wordcloud = WordCloud(stopwords = stop,background_color = 'white',width=2500,height=2000).generate(all)
+        fig, ax = plt.subplots()
+        ax.imshow(wordcloud)
+        ax.axis('off')
 
-    fig, ax = plt.subplots()
-    ax.imshow(wordcloud)
-    ax.axis('off')
-
-    st.pyplot(fig)
+        st.pyplot(fig)
